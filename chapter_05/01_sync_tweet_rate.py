36b2725bd9acf8f137a2d535f422b56f317152b1
Fernando Chimicoviaki <fernando.chimicoviaki@gmail.com>
2016-05-01 10:16:29 -0300

    create chapter 05

---
+++ chapter_05/01_sync_tweet_rate.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+import datetime
+import json
+import time
+import urllib
+
+import tornado.httpserver
+import tornado.httpclient
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        query = self.get_argument('q')
+        client = tornado.httpclient.HTTPClient()
+        response = client.fetch(
+            "https://api.twitter.com/1.1/search/tweets.json" +
+            urllib.urlencode({
+                "q": query, "result_type": "recent", "rpp": 100}))
+        body = json.loads(response.body)
+        result_cont = len(body['results'])
+        now = datetime.datetime.utcnow()
+        raw_oldest_tweet_at = body['results'][-1]['created_at']
+        oldest_tweet_at = datetime.datetime.strptime(
+            raw_oldest_tweet_at, "%a, %d %b %Y %H %M %S +0000")
+        seconds_diff = time.mktime(oldest_tweet_at.timetuple())
+        tweets_per_second = float(result_cont) / seconds_diff
+        self.write("""
+            <div style="text-align: center">
+                <div style="font-size: 72px">%s</div>
+                <div style="font-size: 144px">%.02f</div>
+                <div style="font-size: 24px">tweets per second</div>
+            </div>
+        """ % (query, tweets_per_second))
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
