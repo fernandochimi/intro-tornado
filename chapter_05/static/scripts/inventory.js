652aece99395e23f9891e65a167b48fc19515148
Fernando Chimicoviaki <fernando.chimicoviaki@gmail.com>
2016-05-05 19:42:40 -0300

    rewrite inventory.js

--- chapter_05/static/scripts/inventory.js
+++ chapter_05/static/scripts/inventory.js
@@ -1,54 +1,55 @@
 $(document).ready(function(){
-	document.session = $("#session").val();
+	document.session = $('#session').val();
 
 	setTimeout(requestInventory, 100);
 
-	$("#add-button").click(function(event){
+	$('#add-button').click(function(event){
 		jQuery.ajax({
-			url: "//localhost:8000/cart",
-			type: "POST",
+			url: '//localhost:8000/cart',
+			type: 'POST',
 			data: {
 				session: document.session,
-				action: "add"
+				action: 'add'
 			},
-			dataType: "json",
+			dataType: 'json',
 			beforeSend: function(xhr, settings) {
-				$(event.target).attr("disabled", "disabled");
+				$(event.target).attr('disabled', 'disabled');
 			},
 			success: function(data, status, xhr) {
-				$("add-to-cart").hide();
-				$("remove-from-cart").show();
-				$(event.target).removeAttr("disabled");
+				$('#add-to-cart').hide();
+				$('#remove-from-cart').show();
+				$(event.target).removeAttr('disabled');
 			}
 		});
 	});
 
-	$("#remove-button").click(function(event){
+	$('#remove-button').click(function(event){
 		jQuery.ajax({
-			url: "//localhost:8000/cart",
-			type: "POST",
+			url: '//localhost:8000/cart',
+			type: 'POST',
 			data: {
 				session: document.session,
-				action: "remove"
+				action: 'remove'
 			},
-			dataType: "json",
+			dataType: 'json',
 			beforeSend: function(xhr, settings) {
-				$(event.target).attr("disabled", "disabled");
+				$(event.target).attr('disabled', 'disabled');
 			},
 			success: function(data, status, xhr) {
-				$("remove-from-cart").hide();
-				$("add-to-cart").show();
-				$(event.target).removeAttr("disabled");
+				$('#remove-from-cart').hide();
+				$('#add-to-cart').show();
+				$(event.target).removeAttr('disabled');
 			}
 		});
 	});
-
-	function requestInventory() {
-		jQuery.getJSON("//localhost:8000/cart/status", {session: document.session},
-			function(data, status, xhr) {
-				$("#count").html(data["inventoryCount"]);
-				setTimeout(requestInventory, 0);
-			}
-		);
-	}
 });
+
+function requestInventory() {
+	jQuery.getJSON('//localhost:8000/cart/status', {session: document.session},
+		function(data, status, xhr) {
+			$('#count').html(data['inventoryCount']);
+			console.log(data['inventoryCount']);
+			setTimeout(requestInventory, 0);
+		}
+	);
+}
